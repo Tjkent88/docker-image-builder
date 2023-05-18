949f629f70b8c4fa0f7be752f1e328ae03eb4bf5
Ted Kent <tjkent88@gmail.com>
2023-05-18 16:34:46 +0000

    first commit

---
+++ build-image.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Variables
+REPO_URL="$1"
+IMAGE_NAME="$2"
+DOCKERHUB_USERNAME="tjkent88"
+
+# Clone the repository
+git clone $REPO_URL ~tkent/tmp/
+cd ~tkent/tmp/
+
+# Get the current date and time
+DATE=$(date +%Y%m%d%H%M%S)
+
+# Confirm docker is logged in
+docker login
+
+# Build the Docker image
+docker build -t $IMAGE_NAME:$DATE .
+
+# Tag the Docker image as latest
+docker tag $IMAGE_NAME:$DATE $DOCKERHUB_USERNAME/$IMAGE_NAME:latest
+
+# Push the Docker image with the date tag
+docker push $DOCKERHUB_USERNAME/$IMAGE_NAME:$DATE
+
+# Push the Docker image to Docker Hub
+docker push $DOCKERHUB_USERNAME/$IMAGE_NAME:latest
+
+# Clean up
+cd ..
+rm -rf ~tkent/tmp
